bc82da7d8a1f28f81f648251f971aab41951deb4
Nam <namph2102@gmail.com>
2023-01-08 20:13:57 +0700

    cetae

---
+++ js.js
@@ -0,0 +1,25 @@
+const arr=[
+    {
+        name:'quanao',
+        total:100
+    },
+    {
+        name:'quanao',
+        total:110
+    },
+    {
+        name:'quanao',
+        total:130
+    },
+    {
+        name:'quanao',
+        total:10
+    },
+    {
+        name:'quanao',
+        total:60
+    },
+]
+const newArr=[1,8,5,3,6,9,122,14,20,10,2,8]
+newArr.sort((a,b)=>b-a)
+console.log(newArr);
